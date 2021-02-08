77b827181e78fb41d7dbbcf8232e1a1a29f4cc57
purushottam <pipalpurus@gmail.com>
2021-02-08 21:56:24 +0545

    changes in directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,26 +11,26 @@ class Directory extends React.Component{
                   title: 'Hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'hats'
+                  linkUrl: 'shop/hats'
                 },
                 {
                   title: 'Jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'jackets'
+                  linkUrl: 'shop/jackets'
                 },
                 {
                   title: 'Sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'sneakers'
+                  linkUrl: 'shop/sneakers'
                 },
                 {
                   title: 'Womens',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'womens'
+                  linkUrl: 'shop/womens'
                 },
                 {
                   title: 'Mens',
