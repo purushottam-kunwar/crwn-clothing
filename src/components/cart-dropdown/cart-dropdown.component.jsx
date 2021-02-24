49d9de30c03f628e78c0f3c43370f936028ca045
purushottam <pipalpurus@gmail.com>
2021-02-24 17:26:44 +0545

    implementation redux in card

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,11 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => {
-  <div className="cart-dropdown"></div>;
-};
+const CartDropdown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items" />
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
