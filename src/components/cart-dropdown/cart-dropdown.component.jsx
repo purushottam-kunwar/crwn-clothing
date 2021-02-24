7fcb1f72527e0e87f2fd1e0689fdc56750cf7ec2
purushottam <pipalpurus@gmail.com>
2021-02-24 16:54:42 +0545

    cart dropdown component added

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+  <div className="cart-dropdown"></div>;
+};
