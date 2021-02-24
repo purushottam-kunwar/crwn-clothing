e8d9834ed89f3fb85948a076ccdf92cc0b53f741
purushottam <pipalpurus@gmail.com>
2021-02-24 16:34:42 +0545

    user redirect and user action updated

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { auth } from '../../firebase/firebase.util';
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
