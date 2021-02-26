85c12c26285a1e9843d668684d6b760141fa3bad
purushottam <pipalpurus@gmail.com>
2021-02-26 11:16:38 +0545

    added chaeckout page and checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import {
   selectCartItems,
   selectCartTotal
@@ -27,10 +30,10 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => cartItem.name)}
-    <div className="total">
-      <span>Total: ${total}</span>
-    </div>
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">TOTAL: ${total}</div>
   </div>
 );
 
